17f1b78295f7cb0d9ebb710169e32dce9c7cb184
Ayrton Rodrigues <ayrtonrodrigues45@gmail.com>
2022-09-15 17:52:50 -0300

    subindo v0.2

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,15 @@
 from django.views.generic import TemplateView
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
 class IndexView(TemplateView):
-    template_name = "ProjetoCafe.html"
+    template_name = "Fab2022.html"
+
+def chiclete(request):
+    html = "<html><body><h1>Tome chiclete</h1></body></html>"
+    return HttpResponse(html)
+
+def jujuba(request):
+     html = "<html><body><h1>Tome jujuba</h1></body></html>"
+     return HttpResponse(html)
